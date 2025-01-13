2460c3b12de77fb5bb0c613004422eaf1e2eeb4d
abstrct <abstrct@slow.ninja>
2025-01-13 13:49:20 -0500

    Role Management via Manager and Agent in place.

--- scripts/role-agent.sh
+++ scripts/role-agent.sh
@@ -1,66 +1,60 @@
 #!/usr/bin/env bash
 
 # Variables
-ACCOUNT_WAIT_SLEEP=10
+ROLE_AGENT_SLEEP=30
 CLIENT_FLAGS="--gas auto --yes=true"
 
-# Come online
-  # Great work! Keep this up and you'll be promoted to Senior Signer
 
 # Look for the transaction json blob in the tmp folder
-if [ -e /var/structs/tsa/tmp/tx_$1.json ]; then
-  echo "AGENT($BASHPID): I can't work under these conditions! Where the heck is my transaction ${1} "
+if [ -e /var/structs/tsa/tmp/role_$1.json ]; then
+  echo "ROLE AGENT($BASHPID): I can't work under these conditions! Where the heck is my role ${1} "
   exit
 fi
 
 # Extract details from the JSON blob into variables
-echo "AGENT($BASHPID): Reviewing Transaction Details"
+echo "ROLE AGENT($BASHPID): Reviewing Role Details"
 
-PENDING_TRANSACTION_JSON=$(cat /var/structs/tsa/tmp/tx_$1.json)
-PENDING_TRANSACTION_ID=$( echo ${PENDING_TRANSACTION_JSON} | jq ".id" )
-PENDING_OBJECT_ID=$( echo ${PENDING_TRANSACTION_JSON} | jq ".object_id" )
-PENDING_COMMAND=$( echo ${PENDING_TRANSACTION_JSON} | jq ".command" )
+STUB_ROLE_JSON=$(cat /var/structs/tsa/tmp/tx_$1.json)
+STUB_ROLE_ID=$( echo ${STUB_ROLE_JSON} | jq ".id" )
+STUB_ROLE_GUILD_ID=$( echo ${STUB_ROLE_JSON} | jq ".guild_id" )
 
-echo "AGENT($BASHPID): TX_ID(${PENDING_TRANSACTION_ID}) OBJECT_ID(${PENDING_OBJECT_ID}) COMMAND(${PENDING_COMMAND})"
-until [[ ! -z "${PENDING_ACCOUNT_ADDRESS}" ]]
+echo "ROLE AGENT($BASHPID): ROLE_ID(${STUB_ROLE_ID}) GUILD_ID(${PENDING_OBJECT_ID})"
+
+
+# Create the new primary Role address
+TEMP_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
+MNEMONIC=$(structsd keys add "$TEMP_NAME" | jq ".mnemonic")
+
+# Get the address of what was just added
+ACCOUNT_ADDRESS=$(structsd keys show "$TEMP_NAME" | jq -r ".address" )
+
+# rename the account to the role account id
+structsd keys rename $TEMP_NAME account_$ACCOUNT_ADDRESS
+
+# TSA sign a proxy-join message
+SIGNED_PROXY_JSON=$(structs-sign-proxy guild-join ${STUB_ROLE_GUILD_ID} 0 "$MNEMONIC")
+SIGNED_PROXY_PUBKEY=$( echo ${SIGNED_PROXY_JSON} | jq ".pubkey" )
+SIGNED_PROXY_SIGNATURE=$( echo ${SIGNED_PROXY_JSON} | jq ".signature" )
+
+echo "ROLE AGENT($BASHPID): Signature for Role Proxy Join ${ACCOUNT_ADDRESS} ${SIGNED_PROXY_PUBKEY} ${SIGNED_PROXY_SIGNATURE}"
+
+# Set the primary address of the pending internal account
+psql -c "SELECT signer.SET_PLAYER_INTERNAL_PENDING_PRIMARY_ADDRESS('${STUB_ROLE_ID}','${ACCOUNT_ADDRESS}');" --no-align -t
+
+# Setup the Pending account
+psql -c "SELECT signer.CREATE_PENDING_ACCOUNT_FROM_ROLE('${STUB_ROLE_ID}','${ACCOUNT_ADDRESS}');" --no-align -t
+
+
+# Create a Join Proxy message for the new account
+# 16 represents the Association permission needed on the guild object
+NEW_ROLE_TRANSACTION_JSON=$(psql -c "SELECT signer.CREATE_TRANSACTION('${STUB_ROLE_GUILD_ID}',16,'guild-membership-join-proxy',jsonb_build_array('${ACCOUNT_ADDRESS}','${SIGNED_PROXY_PUBKEY}','${SIGNED_PROXY_SIGNATURE}'),'{}');" --no-align -t)
+
+
+# Wait for the address to show up in the permissions table
+until [ $ADDRESS_COUNT -gt 0 ]; then
 do
-
-  PENDING_TRANSACTION_ACCOUNT_JSON=$( psql -c 'select signer.CLAIM_INTERNAL_ACCOUNT($1);' --no-align -t)
-  PENDING_ACCOUNT_ADDRESS=$( echo ${PENDING_TRANSACTION_ACCOUNT_JSON} | jq '.address' )
-
-  if [ -z "${PENDING_ACCOUNT_ADDRESS}" ]; then
-    echo "AGENT($BASHPID): Heading to the breakroom, unable to find an account"
-    sleep $ACCOUNT_WAIT_SLEEP
-  fi
+  sleep $ROLE_AGENT_SLEEP
+  ADDRESS_COUNT=$( psql -c "select count(1) from structs.permission WHERE object_index = '${ACCOUNT_ADDRESS}';" --no-align -t)
 done
 
-PENDING_ACCOUNT_ID=$( echo ${PENDING_TRANSACTION_ACCOUNT_JSON} | jq '.id' )
-echo "AGENT($BASHPID): Account ${PENDING_ACCOUNT_ID} ${PENDING_ACCOUNT_ADDRESS}"
-
-PENDING_ACCOUNT_NAME="account_${PENDING_ACCOUNT_ADDRESS}"
-
-#{
-#  "id": 1,
-#  "role_id": 1,
-#  "account_id": 1,
-#  "command": "moo",
-#  "args": [],
-#  "flags": [],
-#  "status": "pending",
-#  "output: "",
-#  "created_at": "2024-11-29T16:58:50.410149+00:00",
-#  "updated_at": "2024-11-29T16:58:50.410149+00:00"
-#}
-
-PENDING_ARGS=$(echo ${PENDING_TRANSACTION_ACCOUNT_JSON} | jq -r '.args | to_entries[] | "\"\(.value)\""' |  tr '\n' ' ')
-PENDING_FLAGS=$(echo ${PENDING_TRANSACTION_ACCOUNT_JSON} | jq -r '.flags | to_entries[] | "--\(.key)=\"\(.value)\""' |  tr '\n' ' ')
-
-echo "AGENT($BASHPID): Signing.... structsd tx structs ${PENDING_COMMAND} ${PENDING_ARGS} ${PENDING_FLAGS} --from ${PENDING_ACCOUNT_NAME} ${CLIENT_FLAGS}"
-BROADCAST_RESULT=$(structsd tx structs ${PENDING_COMMAND} ${PENDING_ARGS} ${PENDING_FLAGS} --from ${PENDING_ACCOUNT_NAME} ${CLIENT_FLAGS})
-
-echo "AGENT($BASHPID): ${BROADCAST_RESULT}"
-
-#psql -c 'select signer.TRANSACTION_ERROR(transaction_id INTEGER, transaction_error TEXT);' --no-align -t
-
-psql -c 'select signer.TRANSACTION_BROADCAST_RESULTS(${PENDING_TRANSACTION_ID}, ${BROADCAST_RESULT});' --no-align -t
-
+psql -c "UPDATE signer.account SET status='available' WHERE address = '${ACCOUNT_ADDRESS}';" --no-align -t
--- scripts/role-manager.sh
+++ scripts/role-manager.sh
@@ -1,36 +1,22 @@
 #!/usr/bin/env bash
 
 # Variables
-SLEEP=30
+ROLE_MANAGER_SLEEP=10
 
-
-# Come Online
-  # Done. Great work team.
-
-# Report to the DB about being online
-  # No place to put this yet
-
-# Check to see if the TSA has been initialized
-echo "Loading TSA Signing Manager"
-until [ -e /var/structs/tsa/ready ]
-do
-  echo "Waiting for readiness check. Have you run role-init.sh?"
-  sleep 10
-done
-
+echo "Loading TSA Role Manager"
 
 while :
 do
-  PENDING_TRANSACTION=$(psql -c 'select signer.CLAIM_INTERNAL_TRANSACTION();' --no-align -t)
-  PENDING_TX_ID=$(echo $PENDING_TRANSACTION | jq -r '.id')
+  STUB_ROLE=$(psql -c 'select signer.CLAIM_ROLE_STUB();' --no-align -t)
+  STUB_ROLE_ID=$(echo $STUB_ROLE | jq -r '.id')
 
-  if [[ ! -z "${PENDING_TX_ID}" ]]; then
-    echo $PENDING_TRANSACTION > /var/structs/tsa/tmp/tx_${PENDING_TX_ID}.json
+  if [[ ! -z "${STUB_ROLE_ID}" ]]; then
+    echo $STUB_ROLE > /var/structs/tsa/tmp/role_${STUB_ROLE_ID}.json
 
-    echo "Launching Agent Minion for Transaction ${PENDING_TX_ID}"
-    ./role-agent.sh "${PENDING_TX_ID}" &
+    echo "Launching Agent Minion for Role Generation ${STUB_ROLE_ID}"
+    ./role-agent.sh "${STUB_ROLE_ID}" &
   else
-      sleep $SLEEP
+      sleep $ROLE_MANAGER_SLEEP
   fi
 
 done
