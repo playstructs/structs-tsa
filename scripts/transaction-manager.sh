69807cb9d98df585a45509eacd5b694925536a43
abstrct <abstrct@slow.ninja>
2025-01-14 10:08:04 -0500

    bash bug with psql returning the word null

--- scripts/transaction-manager.sh
+++ scripts/transaction-manager.sh
@@ -9,13 +9,15 @@ do
   PENDING_TRANSACTION=$(psql $DATABASE_URL -c 'select signer.CLAIM_INTERNAL_TRANSACTION();' --no-align -t)
   PENDING_TX_ID=$(echo $PENDING_TRANSACTION | jq -r '.id')
 
-  if [ "$PENDING_TX_ID" != "null" ]; then
-    echo $PENDING_TRANSACTION > /var/structs/tsa/tmp/tx_${PENDING_TX_ID}.json
+  if [[ ! -z "$PENDING_TRANSACTION" ]]; then
+    if [ "$PENDING_TX_ID" != "null" ]; then
+      echo $PENDING_TRANSACTION > /var/structs/tsa/tmp/tx_${PENDING_TX_ID}.json
 
-    echo "TX MANAGER: Launching Agent Minion for Transaction ${PENDING_TX_ID}"
-    bash /src/structs/transaction-agent.sh "${PENDING_TX_ID}" &
-  else
-      sleep $TRANSACTION_MANAGER_SLEEP
+      echo "TX MANAGER: Launching Agent Minion for Transaction ${PENDING_TX_ID}"
+      bash /src/structs/transaction-agent.sh "${PENDING_TX_ID}" &
+    else
+        sleep $TRANSACTION_MANAGER_SLEEP
+    fi
   fi
 
 done
